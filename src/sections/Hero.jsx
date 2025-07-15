e83758b60580f0c749952abcf4ac3fe35aa260c0
Cihan Bekem <147487665+cihanbekem@users.noreply.github.com>
2025-07-15 21:49:53 +0300

    Add files via upload

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -56,7 +56,7 @@ const Hero = () => {
           initial={{ opacity: 0, y: 50 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8 }}
-          className="space-y-8 w-full flex flex-col items-center justify-center"
+          className="space-y-4 w-full flex flex-col items-center justify-center"
         >
           {/* Logo */}
           <motion.img
